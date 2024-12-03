08ca703c1aac3bcf51a4c5d6a333ab4b2aa3da50
Jeroen Weener <jweener1996@gmail.com>
2024-12-03 07:54:17 +0100

    Add solutions to 2024.03

---
+++ 2024/day03/app/Main.hs
@@ -0,0 +1,40 @@
+import Text.Regex.TDFA
+
+-- Run `cabal run` to execute this solution.
+main :: IO ()
+main = do
+  example <- readFile "example.txt"
+  example2 <- readFile "example2.txt"
+  input <- readFile "input.txt"
+
+  putStrLn "Part 1"
+  print $ part1 example
+  print $ part1 input
+  putStrLn "\nPart 2"
+  print $ part2 example2
+  print $ part2 input
+
+regex :: String
+regex = "mul\\([0-9]+,[0-9]+\\)"
+
+regex2 :: String
+regex2 = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)"
+
+part1 :: String -> Int
+part1 input = sum . map parseMul $ getAllTextMatches (input =~ regex)
+
+part2 :: String -> Int
+part2 input = parseInstructions True $ getAllTextMatches (input =~ regex2)
+
+parseMul :: String -> Int
+parseMul text = a * b
+  where
+    a = read $ takeWhile (/= ',') $ drop 4 text
+    b = read $ init $ drop 1 $ dropWhile (/= ',') text
+
+parseInstructions :: Bool -> [String] -> Int
+parseInstructions _ [] = 0
+parseInstructions _ ("do()" : ts) = parseInstructions True ts
+parseInstructions _ ("don't()" : ts) = parseInstructions False ts
+parseInstructions True (mul : ts) = parseMul mul + parseInstructions True ts
+parseInstructions False (mul : ts) = parseInstructions False ts
