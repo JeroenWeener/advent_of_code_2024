1bd2f0a9fa9168d7b6211dfacd6d55b93cf8a7dd
Jeroen Weener <jweener1996@gmail.com>
2024-12-11 20:28:43 +0100

    Improve code for day 11

--- src/Day11.hs
+++ src/Day11.hs
@@ -1,10 +1,6 @@
 module Day11 where
 
-import Data.Map ((!))
-import qualified Data.Map as Map
-import Data.Maybe (fromMaybe)
-
-type Cache = Map.Map Int (Map.Map Int Int)
+import Data.MemoTrie (memo2)
 
 solveDay11 :: IO ()
 solveDay11 = do
@@ -22,45 +18,22 @@ parseInput :: String -> [Int]
 parseInput = map read . words
 
 part1 :: [Int] -> Int
--- part1 input = length $ iterate (concatMap blink) input !! 25
-part1 = sum . map (fst . blinkCached 25 Map.empty)
+part1 = sum . map (flutter 25)
 
 part2 :: [Int] -> Int
-part2 = sum . map (fst . blinkCached 75 Map.empty)
+part2 = sum . map (flutter 75)
 
 blink :: Int -> [Int]
-blink stone
-  | stone == 0 = [1]
-  | even . length $ show stone = [read l, read r]
-  | otherwise = [stone * 2024]
+blink s
+  | s == 0 = [1]
+  | (even . length) t = [read l, read r]
+  | otherwise = [s * 2024]
   where
-    s = show stone
-    (l, r) = splitAt (length s `div` 2) s
+    t = show s
+    (l, r) = splitAt (length t `div` 2) t
 
-cacheContains :: Int -> Int -> Cache -> Bool
-cacheContains depth stone cache =
-  Map.member stone cache
-    && Map.member depth (cache ! stone)
-
-cacheInsert :: Int -> Int -> Int -> Cache -> Cache
-cacheInsert depth stone size cache = cache'
+flutter :: Int -> Int -> Int
+flutter = memo2 flutter'
   where
-    subcache = fromMaybe Map.empty (Map.lookup stone cache)
-    subcache' = Map.insert depth size subcache
-    cache' = Map.insert stone subcache' cache
-
-blinkCached :: Int -> Cache -> Int -> (Int, Cache)
-blinkCached depth cache stone
-  | depth == 0 = (1, cacheInsert 0 stone 1 cache)
-  | cacheContains depth stone cache = (cache ! stone ! depth, cache)
-  | otherwise = (totalSize, updatedCache')
-  where
-    (totalSize, updatedCache) =
-      foldl
-        ( \(subtotalSize, cache') substone ->
-            let (subsize, cache'') = blinkCached (depth - 1) cache' substone
-             in (subsize + subtotalSize, cache'')
-        )
-        (0, cache)
-        (blink stone)
-    updatedCache' = cacheInsert depth stone totalSize updatedCache
+    flutter' 0 _ = 1
+    flutter' d s = sum $ map (flutter (d - 1)) (blink s)
